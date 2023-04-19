87eaee78ffbbb76c711c6dceb9f4271becd36fa5
liveisliiife <127602972+liveisliiife@users.noreply.github.com>
2023-04-20 00:35:00 +0300

    Add files via upload

---
+++ selenium_instagram_giris_botu.py
@@ -0,0 +1,25 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+username = input("Kullanıcı Adı: ")
+password = input("Sifre: ")
+
+driver = webdriver.Chrome("D:/ceng111/pythonProject/chromedriver.exe")
+url = "https://www.instagram.com/accounts/login/"
+driver.get(url)
+
+giris = driver.find_element(By.XPATH,"//*[@id='loginForm']/div/div[1]/div/label/input")
+giris.send_keys(username)
+
+sifre = driver.find_element(By.XPATH,"//*[@id='loginForm']/div/div[2]/div/label/input")
+sifre.send_keys(password)
+
+ok = driver.find_element(By.XPATH,"//*[@id='loginForm']/div/div[3]")
+ok.click()
+
+time.sleep(1)
+driver.maximize_window()
+
+input("Programın kapatılması için bir tuşa basın...")
+driver.close()
